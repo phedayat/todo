c2bb7d85e772d8a5644349d8563df98aa841741a
Parsia <phedayat@Parsias-iMac.local>
2020-07-13 22:29:35 -0500

    Added number of items function and main TODO controller

---
+++ todo_main.py
@@ -0,0 +1,28 @@
+from todo_store import TodoStore
+from todo_item import TodoItem
+
+mainStore = TodoStore()
+
+command = input("What would you like to do? ")
+
+while command != "quit":
+    if command == "add":
+        priority = input("Priority: ")
+        description = input("Description: ")
+        isDue = input("Is it due (True/False): ")
+
+        if isDue:
+            due = input("Due date: ")
+        
+        item = TodoItem(description, priority=priority, isDue=isDue)
+        mainStore.addItem(item)
+    elif command == "complete":
+        itemNum = input("Enter item number: ")
+
+        mainStore.completeItem(itemNum)
+    elif command == "export":
+        mainStore.exportList()
+    elif command == "total":
+        print(f"Number of items: {mainStore.getNumItems()}")
+
+exit(0)
--- todo_store.py
+++ todo_store.py
@@ -7,8 +7,12 @@ class TodoStore:
         self.todoListFile = None
         self.todoList = []
 
+    def getNumItems(self):
+        return self.numItems
+
     def addItem(self, todoItem):
         self.todoList.append(todoItem)
+        self.numItems += 1
     
     def completeItem(self, numItem):
         self.todoList[numItem].setCompleted(True)
@@ -21,5 +25,5 @@ class TodoStore:
                 self.todoListFile.write(f"[x] {item.getPriority()}: {item.getDescription()}; Due: {(item.isDue, item.due)[item.isDue]}")
             else:
                 self.todoListFile.write(f"[ ] {item.getPriority()}: {item.getDescription()}; Due: {(item.isDue, item.due)[item.isDue]}")
-                
+
         self.todoListFile.close()
